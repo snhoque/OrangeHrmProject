e1b99703d1df27e32be7dee1c0489aed22952af7
snhoque <shoque23@gmail.com>
2022-08-24 21:05:45 -0400

    Added page

--- OrangeHrmProject/src/main/java/pages/PIMPage.java
+++ OrangeHrmProject/src/main/java/pages/PIMPage.java
@@ -14,4 +14,34 @@ import java.util.List;
 import static browser.Browser.driver;
 
 public class PIMPage {
+    WebDriver driver = Browser.driver;
+
+    @FindBy(linkText = "Recruitment") private WebElement recruitmentLink;
+    @FindBy(css = "button[type='submit']") private WebElement submitButton;
+    @FindBy(xpath = "//div[@class=\"oxd-table-row oxd-table-row--with-border oxd-table-row--clickable\"]/div[3]/div")
+    private WebElement firstNameElement;
+    @FindBy(xpath = "//div[@class=\"oxd-table-row oxd-table-row--with-border oxd-table-row--clickable\"]/div[4]/div")
+    private WebElement lastNameElement;
+
+
+
+    public RecruitmentPage navigateToRecruitmentPage() {
+        recruitmentLink.click();
+        return PageFactory.initElements(Browser.driver, RecruitmentPage.class);
+    }
+
+    public PIMPage searchEmployeeId(String id) {
+        List<WebElement> elements = driver.findElements(By.cssSelector(".oxd-input.oxd-input--active"));
+        elements.get(1).sendKeys(id);
+        submitButton.click();
+        return this;
+    }
+
+    public PIMPage validateFirstAndLastName(String firstName, String lastName) {
+
+        Assert.assertEquals(firstNameElement.getText(), firstName);
+        Assert.assertEquals(lastNameElement.getText(), lastName);
+        return this;
+    }
+
 }
